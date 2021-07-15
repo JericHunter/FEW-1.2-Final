4ee8714ab80032ea8f0d37e817cbfa70ea22cdde
JericHunter <jeric.hunterr@gmail.com>
2021-07-15 16:25:25 -0700

    job finished

---
+++ src/components/Heading.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+function Heading(props) {
+    return (
+        <div>
+            <h1>Jeric H</h1>
+            <h3>FEW 1.2 Final Assessment</h3>
+        </div>
+    );
+}
+
+export default Heading;
